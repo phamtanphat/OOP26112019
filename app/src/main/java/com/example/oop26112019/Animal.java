71c1c8e221f9ccff1d67173c1fa4ca05d8aeb3f9
phatdroid94 <phatdroid94@gmail.com>
2019-12-24 18:20:42 +0700

    constructor extend

--- app/src/main/java/com/example/oop26112019/Animal.java
+++ app/src/main/java/com/example/oop26112019/Animal.java
@@ -5,6 +5,11 @@ public class Animal {
     String color;
     String speak;
 
+    public Animal(String name , String color){
+        this.name = name;
+        this.color = color;
+    }
+
     public void setLanguage(String language){
         this.speak = language;
     }
--- app/src/main/java/com/example/oop26112019/Meo.java
+++ app/src/main/java/com/example/oop26112019/Meo.java
@@ -1,6 +1,19 @@
 package com.example.oop26112019;
 
+import android.util.Log;
+
 public class Meo extends Animal {
+    String address;
     // 5 : solid
 
+    public Meo(String name , String color , String address){
+        super(name,color);
+        this.address = address;
+    }
+
+    @Override
+    public void setLanguage(String language) {
+        Log.d("BBB","Intance Meo");
+        super.setLanguage(language);
+    }
 }
